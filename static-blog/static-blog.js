7f476604eebcd11ee9dd00066e4289d3bc0fcd6d
john afiukwa <johnafiukwa2011@gmail.com>
2022-08-06 11:17:37 +0100

    this

--- static-blog/static-blog.js
+++ static-blog/static-blog.js
@@ -3,15 +3,38 @@ const blogContainer = document.getElementById("blog");
 const fetchPosts = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await response.json();
-  console.log(posts);
+  // console.log(posts);
 
-  posts.forEach(post => {
-    const titleContainer = document.createElement('p');
+  posts.forEach((post) => {
+    const eachBlog = document.createElement("div");
+    eachBlog.setAttribute("class", "each-blog");
+
+    const titleContainer = document.createElement("div");
+    titleContainer.setAttribute("class", "title-container");
     titleContainer.textContent = post.title;
-    blogContainer.appendChild(titleContainer);
+
+    eachBlog.appendChild(titleContainer);
+    blogContainer.appendChild(eachBlog);
+  });
+};
+
+fetchPosts();
+
+const fetchUsers = async () => {
+  const eachBlog = document.createElement("div");
+    eachBlog.setAttribute("class", "each-blog");
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  console.log(users);
+
+  users.forEach((post) => {
+    const userContainer = document.createElement("div");
+    userContainer.setAttribute("class", "user-container");
+    userContainer.textContent = post.name;
+
+    eachBlog.appendChild(userContainer);
   });
 }
 
 
-fetchPosts();
-
+fetchUsers();
