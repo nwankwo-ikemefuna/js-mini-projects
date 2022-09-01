cd3ea3bcbf44709cadc04d82ffa3a40221b4d5c4
john afiukwa <johnafiukwa2011@gmail.com>
2022-09-01 23:32:05 +0100

    Done with the layouts

--- projects/display-project.js
+++ projects/display-project.js
@@ -12,18 +12,26 @@ const studentDepartment = getDepartmentById(viewedProject.departmentId);
 
 const studentSupervisor = getSupervisorById(viewedProject.superviorId);
 
-const displayProjectContainer = document.getElementById("view-project-container");
+const displayProjectContainer = document.getElementById(
+  "view-project-container"
+);
 
 const displayStudentHolder = document.getElementById("display-student-holder");
-const displayDepartmentHolder = document.getElementById("display-depatment-holder");
-const displaySupervisorHolder = document.getElementById("display-supervisor-holder");
+const displayDepartmentHolder = document.getElementById(
+  "display-depatment-holder"
+);
+const displaySupervisorHolder = document.getElementById(
+  "display-supervisor-holder"
+);
 const displayTitleHolder = document.getElementById("display-title-holder");
 const displayContentHolder = document.getElementById("display-content-holder");
-const displayDateOfSubmissionHolder = document.getElementById("display-date-of-submission-holder");
+const displayDateOfSubmissionHolder = document.getElementById(
+  "display-date-of-submission-holder"
+);
 
-displayStudentHolder.innerHTML = `<b>Name:</b> ${studentName.name}`
-displayDepartmentHolder.innerHTML = `<b>Department:</b> ${studentDepartment.name}`
-displaySupervisorHolder.innerHTML = `<b>Supervisor:</b> ${studentSupervisor.name}`
+displayStudentHolder.innerHTML = `<b>Name:</b> ${studentName.name}`;
+displayDepartmentHolder.innerHTML = `<b>Department:</b> ${studentDepartment.name}`;
+displaySupervisorHolder.innerHTML = `<b>Supervisor:</b> ${studentSupervisor.name}`;
 displayTitleHolder.innerHTML = `<b>Title:</b> ${viewedProject.title}`;
 displayContentHolder.innerHTML = `<b>Content:</b> ${viewedProject.content}`;
 displayDateOfSubmissionHolder.innerHTML = `<b>Submitted on:</b> ${viewedProject.dateOfSubmission}`;
